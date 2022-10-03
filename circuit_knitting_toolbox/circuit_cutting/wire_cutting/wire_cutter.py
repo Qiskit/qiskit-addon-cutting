98441cfe7c784813de2f6bc469f41066258f7c6c
Caleb Johnson <calebj1524@outlook.com>
2022-10-03 18:35:41 -0500

    Rework circuit cutting workflow

--- circuit_knitting_toolbox/circuit_cutting/wire_cutting/wire_cutter.py
+++ circuit_knitting_toolbox/circuit_cutting/wire_cutting/wire_cutter.py
@@ -6,7 +6,7 @@ from nptyping import NDArray
 from .wire_cutting import find_wire_cuts, cut_circuit_wire
 from .wire_cutting_evaluation import run_subcircuit_instances
 from .wire_cutting_post_processing import generate_summation_terms, build
-from .wire_cutting_verification import verify
+from .wire_cutting_verification import verify, generate_reconstructed_output
 
 
 class WireCutter:
@@ -68,43 +68,53 @@ class WireCutter:
         cuts: Dict[str, Any],
         mode: str,
         num_shots_fn: Callable,
-        mem_limit: int,
-        num_threads: int,
-    ) -> Tuple[NDArray, List[int]]:
+    ) -> Dict[int, Dict[int, NDArray]]:
         """
         cuts: results from cutting routine
         mode = qasm: simulate shots
         mode = sv: statevector simulation
         num_shots_fn: a function that gives the number of shots to take for a given circuit
         """
-        (
-            summation_terms,
-            subcircuit_entries,
-            subcircuit_instances,
-        ) = _generate_metadata(cuts)
+        _, _, subcircuit_instances = _generate_metadata(cuts)
+
         subcircuit_instance_probs = _run_subcircuits(
             cuts, subcircuit_instances, mode, num_shots_fn
         )
+
+        return subcircuit_instance_probs
+
+    def recompose(
+        self,
+        subcircuit_instance_probs: Dict[int, Dict[int, NDArray]],
+        cuts: Dict[str, Any],
+        num_threads: int = 1,
+    ) -> NDArray:
+        summation_terms, subcircuit_entries, _ = _generate_metadata(cuts)
+
         subcircuit_entry_probs = _attribute_shots(
             subcircuit_entries, subcircuit_instance_probs
         )
         unordered_probability, smart_order = _build(
-            cuts, summation_terms, subcircuit_entry_probs, mem_limit, num_threads
+            cuts, summation_terms, subcircuit_entry_probs, num_threads
         )
-        return unordered_probability, smart_order
+
+        ordered_probability = generate_reconstructed_output(
+            self.circuit,
+            cuts["subcircuits"],
+            unordered_probability,
+            smart_order,
+            cuts["complete_path_map"],
+        )
+
+        return ordered_probability
 
     def verify(
         self,
-        cuts: Dict[str, Any],
-        unordered_probability: Sequence[float],
-        smart_order: Sequence[int],
-    ) -> Tuple[NDArray, Dict[str, Dict[str, float]]]:
-        ordered_probability, metrics = verify(
-            full_circuit=self.circuit,
-            unordered=unordered_probability,
-            complete_path_map=cuts["complete_path_map"],
-            subcircuits=cuts["subcircuits"],
-            smart_order=smart_order,
+        ordered_probability: NDArray,
+    ) -> Dict[str, Dict[str, float]]:
+        metrics = verify(
+            self.circuit,
+            ordered_probability,
         )
         return ordered_probability, metrics
 
@@ -201,7 +211,6 @@ def _build(
     cuts: Dict[str, Any],
     summation_terms: Sequence[Dict[int, int]],
     subcircuit_entry_probs: Dict[int, Dict[int, NDArray]],
-    mem_limit: int,
     num_threads: int,
 ) -> Tuple[NDArray, List[int]]:
     reconstructed_prob, smart_order, overhead = build(
