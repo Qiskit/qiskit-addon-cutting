3094a69a67cca6f1682668c0483b4bc1cbfaedd0
Owen Lockwood <42878312+lockwo@users.noreply.github.com>
2022-10-11 17:39:05 -0600

    Update wire_cutter.py

--- circuit_knitting_toolbox/circuit_cutting/wire_cutting/wire_cutter.py
+++ circuit_knitting_toolbox/circuit_cutting/wire_cutting/wire_cutter.py
@@ -1,3 +1,4 @@
+"""File containing the WireCutter class."""
 from typing import Sequence, Optional, Dict, Any, Tuple, cast, List, Union
 
 from nptyping import NDArray
@@ -20,6 +21,20 @@ from .wire_cutting_verification import verify, generate_reconstructed_output
 
 
 class WireCutter:
+    """Class to hold the main cutting functions.
+
+    This is the main class of user interaction for Wire Cutting. This class
+    manages all the key user functions such as creating, evaluating, and
+    verifying the cuts.
+
+    Attributes:
+        - _circuit (QuantumCircuit): original quantum circuit to be
+            cut into subcircuits
+        - service (QiskitRuntimeService): the runtime service used to simulate or
+            execute the subcircuits on real systems
+        - _options (Options): the options for the qiskit runtime primitives
+    """
+
     def __init__(
         self,
         circuit: QuantumCircuit,
@@ -27,6 +42,21 @@ class WireCutter:
         options: Optional[Options] = None,
         backend_names: Optional[Sequence[str]] = None,
     ):
+        """
+        Assign the necessary member variable.
+
+        Args:
+            - circuit (QuantumCircuit): original quantum circuit to be
+                cut into subcircuits
+            - service (QiskitRuntimeService): the runtime service used for
+                executing the subcircuits
+            - options (Options): options for executing in the session
+            - backend_names (list): the backends to execute the subcircuits 
+                on
+
+        Returns:
+            - None
+        """
         # Set class fields
         self._circuit = circuit
         self.service = service
@@ -34,31 +64,31 @@ class WireCutter:
         self._backend_names = backend_names
 
     @property
-    def circuit(self) -> QuantumCircuit:
+    def circuit(self) -> QuantumCircuit: # noqa: D102
         return self._circuit
 
     @property
-    def service(self) -> Optional[QiskitRuntimeService]:
+    def service(self) -> Optional[QiskitRuntimeService]: # noqa: D102
         return QiskitRuntimeService(**self._service)
 
     @service.setter
-    def service(self, service: Optional[QiskitRuntimeService]) -> None:
+    def service(self, service: Optional[QiskitRuntimeService]) -> None: # noqa: D102
         self._service = service.active_account() if service is not None else service
 
     @property
-    def options(self) -> Optional[Options]:
+    def options(self) -> Optional[Options]: # noqa: D102
         return self._options
 
     @options.setter
-    def options(self, options: Optional[Options]) -> None:
+    def options(self, options: Optional[Options]) -> None: # noqa: D102
         self._options = options
 
     @property
-    def backend_names(self) -> Optional[Sequence[str]]:
+    def backend_names(self) -> Optional[Sequence[str]]: # noqa: D102
         return self._backend_names
 
     @backend_names.setter
-    def backend_names(self, backend_names: Optional[Sequence[str]]) -> None:
+    def backend_names(self, backend_names: Optional[Sequence[str]]) -> None: # noqa: D102
         self._backend_names = backend_names
 
     def decompose(
@@ -71,6 +101,28 @@ class WireCutter:
         max_cuts: Optional[int] = None,
         num_subcircuits: Optional[Sequence[int]] = None,
     ) -> Dict[str, Any]:
+        """
+        Decompose the circuit into a collection of subcircuits.
+
+        Args:
+            - method (str): whether to have the cuts be 'automatic'ally found, in a 
+                provably optimal way, or whether to 'manual'ly provide the cuts
+            - subcircuit_vertices (Sequence[Sequence[int]]): the vertices to be used in 
+                the subcircuits. Note that these are not the indices of the qubits, but
+                the nodes in the circuit DAG
+            - max_subcircuit_width (int): max number of qubits in each subcircuit
+            - max_cuts (int): max total number of cuts allowed
+            - num_subcircuits (Sequence[int]): list of number of subcircuits to try
+            - max_subcircuit_cuts (int, optional): max number of cuts for a subcircuit
+            - max_subcircuit_size (int, optional): max number of gates in a subcircuit
+
+        Returns:
+            - (Dict[str, Any]): A dictionary containing information on the cuts,
+                including the subcircuits themselves (key: 'subcircuits')
+
+        Raises:
+            - ValueError: if the input method does not match the other provided arguments
+        """
         cuts = {}
         if method == "automatic":
             if max_subcircuit_width is None:
@@ -101,7 +153,14 @@ class WireCutter:
 
     def evaluate(self, cuts: Dict[str, Any]) -> Dict[int, Dict[int, NDArray]]:
         """
-        cuts: results from cutting routine
+        Evaluate the subcircuits.
+
+        Args:
+            - cuts (Dict): the results of cutting
+        
+        Returns:
+            - (Dict): the dictionary containing the results from running
+                each of the subcircuits
         """
         _, _, subcircuit_instances = _generate_metadata(cuts)
 
@@ -121,6 +180,19 @@ class WireCutter:
         cuts: Dict[str, Any],
         num_threads: int = 1,
     ) -> NDArray:
+        """
+        Reconstruct the full probabilities from the subcircuit executions.
+
+        Args:
+            - subcircuit_instance_probabilities (dict): the probability vectors from each 
+                of the subcircuit instances, as output by the _run_subcircuits function
+            - cuts (dict): the cuts as found or provided
+            - num_threads (int): number of threads to use to parallelize this operation
+
+        Returns:
+            - (NDArray): the reconstructed probability vector
+        
+        """
         reconstructed_probability_futures = _recompose(
             circuit=self.circuit,
             subcircuit_instance_probabilities=subcircuit_instance_probabilities,
@@ -135,6 +207,20 @@ class WireCutter:
         self,
         reconstructed_probability: NDArray,
     ) -> Dict[str, Dict[str, float]]:
+        """
+        Compare the reconstructed probabilites to the ground truth.
+
+        Note: this function requires the statevector exeuction of the original circuit,
+        making it infeasible for most circuits >25 qubits.
+
+        Args:
+            - reconstructed_probability (NDArray): the econstructed probabilities
+                of the original circuit, obtained from the evaluate method
+
+        Returns:
+            - (Dict): a dictionary containing a variety of metrics comparing the
+                reconstructed probabilities to the ground truth.
+        """
         metrics = verify(
             self.circuit,
             reconstructed_probability,
@@ -149,6 +235,17 @@ def _generate_metadata(
     Dict[int, Dict[Tuple[str, str], Tuple[int, Sequence[Tuple[int, int]]]]],
     Dict[int, Dict[Tuple[Tuple[str, ...], Tuple[Any, ...]], int]],
 ]:
+    """
+    Generate metadata used to execute subcircuits and reconstruct probabilities of original circuit.
+    
+    Args:
+        - cuts (Dict[str, Any]): results from the cutting step
+
+    Returns:
+        - (tuple): information about the 4^(num cuts) summation terms used to reconstruct original
+            probabilities, a dictionary with information on each of the subcircuits, and a dictionary
+            containing indexes for each of the subcircuits
+    """
     (
         summation_terms,
         subcircuit_entries,
@@ -169,8 +266,20 @@ def _run_subcircuits(
     options: Optional[Union[Dict, Options]] = None,
 ) -> Dict[int, Dict[int, NDArray]]:
     """
-    Run all the subcircuit instances
+    Execute all the subcircuit instances.
+
     task['subcircuit_instance_probs'][subcircuit_idx][subcircuit_instance_idx] = measured prob
+
+    Args:
+        - cuts (Dict[str, Any]): results from the cutting step
+        - subcircuit_instances (Dict): the dictionary containing the index information for each
+            of the subcircuit instances
+        - service_args (Dict): the arguments for the runtime service
+        - backend_name (str): the method by which the subcircuits should be run
+        - options (Options): options for the runtime execution of subcircuits
+
+    Returns:
+        - (Dict): the resulting probabilities from each of the subcircuit instances
     """
     subcircuit_instance_probs = run_subcircuit_instances(
         subcircuits=cuts["subcircuits"],
@@ -190,8 +299,22 @@ def _attribute_shots(
     subcircuit_instance_probs: Dict[int, Dict[int, NDArray]],
 ) -> Dict[int, Dict[int, NDArray]]:
     """
-    Attribute the shots into respective subcircuit entries
+    Attribute the shots into respective subcircuit entries.
+
     task['subcircuit_entry_probs'][subcircuit_idx][subcircuit_entry_idx] = prob
+
+    Args:
+        - subcircuit_entries (Dict): dictionary containing information about each of the
+            subcircuit instances
+        - subcircuit_instance_probs (Dict): the probability vectors from each of the subcircuit
+            instances, as output by the _run_subcircuits function
+
+    Returns:
+        - (Dict): a dictionary containing the probability results to each of the appropriate subcircuits
+
+    Raises:
+        - ValueError: if each of the kronecker terms are not of size two or if there are no subcircuit
+            probs provided
     """
     subcircuit_entry_probs: Dict[int, Dict[int, NDArray]] = {}
     for subcircuit_idx in subcircuit_entries:
@@ -238,6 +361,23 @@ def _build(
     subcircuit_entry_probs: Dict[int, Dict[int, NDArray]],
     num_threads: int,
 ) -> Tuple[NDArray, List[int]]:
+    """
+    Complete the final postprocessing to reconstruct probabilities of the original circuit.
+    
+    Args:
+        - cuts (Dict[str, Any]): results from the cutting step
+        - summation_terms (Sequence[Dict[int, int]]): the metadata containing 4^(num cuts) terms
+            which represent the final distribution when summed and combined with the subcircuit probabilities
+        - subcircuit_entry_probs (Dict[int, Dict[int, NDArray]]): the probabilities for each
+            of the subcircuits as calculated from their backend executions
+        - mem_limit (int): memory limit for postprocessing
+        - num_threads (int): number of threads to use for post processing
+
+    Returns:
+        - (tuple): the reconstructed probabilities of the original circuit;
+                the first entry contains the unordered probabilities and the second is the order in
+                which the subcircuit probabilities should be combined
+    """
     reconstructed_prob, smart_order, overhead = build(
         summation_terms=summation_terms,
         subcircuit_entry_probs=subcircuit_entry_probs,
@@ -258,6 +398,18 @@ def _recompose(
     cuts: Dict[str, Any],
     num_threads: int = 1,
 ) -> NDArray:
+    """
+    Reassemble the probability vector using quantum serverless.
+
+    Args:
+        - circuit (QuantumCircuit): the original full circuit
+        - subcircuit_instance_probabilities (dict): the probability vectors from each 
+            of the subcircuit instances, as output by the _run_subcircuits function
+        - num_threads (int): the number of threads to use to parallelize the recomposing
+
+    Returns:
+        - (NDArray): the reconstructed probability vector
+    """
     summation_terms, subcircuit_entries, _ = _generate_metadata(cuts)
 
     subcircuit_entry_probabilities = _attribute_shots(
@@ -288,14 +440,24 @@ def _cut_automatic(
     num_subcircuits: Optional[Sequence[int]] = None,
 ) -> Dict[str, Any]:
     """
-    Automatically find an optimized cut scheme based on input.
+    Automatically find an optimal cut scheme based on input.
 
     Args:
-    max_subcircuit_width: max number of qubits in each subcircuit
-    max_cuts: max total number of cuts allowed
-    max_subcircuit_cuts: max number of cuts for a subcircuit
-    max_subcircuit_size: max number of gates in a subcircuit
-    num_subcircuits: list of subcircuits to try
+        - circuit (QuantumCircuit): the circuit to cut
+        - max_subcircuit_width (int): max number of qubits in each subcircuit
+        - max_cuts (int): max total number of cuts allowed
+        - num_subcircuits (Sequence[int]): list of number of subcircuits to try
+        - max_subcircuit_cuts (int, optional): max number of cuts for a subcircuit
+        - max_subcircuit_size (int, optional): max number of gates in a subcircuit
+
+    Returns:
+        - (Dict[str, Any]): A dictionary containing information on the cuts,
+            including the subcircuits themselves (key: 'subcircuits')
+    
+    Rasies:
+        - ValueError: if the circuit is not provided (during the initialization 
+            of the class)
+        - DOcplexException: if the MIP solver is unable to find the optimal solution
     """
     cuts = find_wire_cuts(
         circuit=circuit,
@@ -315,9 +477,17 @@ def _cut_manual(
     circuit: QuantumCircuit, subcircuit_vertices: Sequence[Sequence[int]]
 ) -> Dict[str, Any]:
     """
-    Cut the given circuits at the wires specified
+    Cut the given circuits at the wires specified.
 
     Args:
+        - circuit (QuantumCircuit): the circuit to cut
+        - subcircuit_vertices (Sequence[Sequence[int]]): the vertices to be used in 
+            the subcircuits. Note that these are not the indices of the qubits, but
+            the nodes in the circuit DAG.
+
+    Returns:
+        - (Dict[str, Any]): A dictionary containing information on the cuts,
+            including the subcircuits themselves (key: 'subcircuits')
     """
     cuts = cut_circuit_wire(
         circuit=circuit,
