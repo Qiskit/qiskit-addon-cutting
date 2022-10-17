6d4596940ac764c830c851f2acc8b49d453ca1ae
Caleb Johnson <calebj1524@outlook.com>
2022-10-17 11:04:52 -0500

    Update license blurbs in all Python files

--- circuit_knitting_toolbox/utils/__init__.py
+++ circuit_knitting_toolbox/utils/__init__.py
@@ -1,14 +1,15 @@
-# This code is part of Qiskit.
-#
+# This code is a Qiskit project.
+
 # (C) Copyright IBM 2022.
-#
+
 # This code is licensed under the Apache License, Version 2.0. You may
 # obtain a copy of this license in the LICENSE.txt file in the root directory
 # of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
-#
 # Any modifications or derivative works of this code must retain this
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
+
 """Code to initialize imports for utils."""
+
 from .integral_driver import IntegralDriver
 from .orbital_reduction import reduce_bitstrings
