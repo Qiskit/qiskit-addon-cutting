df246acbd7ec502dc90dd6273cb8216c88bf1a79
Jim Garrison <garrison@ibm.com>
2022-10-24 15:40:10 -0400

    Add cutting API reference docs

--- circuit_knitting_toolbox/circuit_cutting/__init__.py
+++ circuit_knitting_toolbox/circuit_cutting/__init__.py
@@ -9,6 +9,16 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 
-"""Code to initialize the WireCutter import."""
+"""
+Circuit Cutting (:mod:`circuit_knitting_toolbox.circuit_cutting`).
+
+.. currentmodule:: circuit_knitting_toolbox.circuit_cutting
+
+.. autosummary::
+   :toctree: ../stubs/
+   :nosignatures:
+
+   WireCutter
+"""
 
 from .wire_cutting.wire_cutter import WireCutter
