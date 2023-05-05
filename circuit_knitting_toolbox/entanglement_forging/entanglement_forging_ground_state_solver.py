2d521a724faa40ee13522faf4258ad60a8610031
Caleb Johnson <calebj1524@outlook.com>
2023-05-05 14:56:03 -0500

    We only deal with ESProblem, not BaseProblem

--- circuit_knitting_toolbox/entanglement_forging/entanglement_forging_ground_state_solver.py
+++ circuit_knitting_toolbox/entanglement_forging/entanglement_forging_ground_state_solver.py
@@ -361,19 +361,17 @@ class EntanglementForgingGroundStateSolver:
 
     def get_qubit_operators(
         self,
-        problem: BaseProblem,
+        problem: ElectronicStructureProblem,
     ) -> ListOp:
         """Construct decomposed qubit operators from an ``ElectronicStructureProblem``.
 
         Args:
-          - problem (BaseProblem): A class encoding a problem to be solved.
+          - problem (ElectronicStructureProblem): A class encoding a problem to be solved.
 
         Returns:
           - hamiltonian_ops: qubit operator representing the decomposed Hamiltonian.
 
         Raises:
-            - ValueError:
-                Input problem was not an instance of ``ElectronicStructureProblem``
             - ValueError:
                 The input problem is not in MO basis, and ``mo_coeff`` is set to ``None``
             - ValueError:
@@ -389,12 +387,8 @@ class EntanglementForgingGroundStateSolver:
         return hamiltonian_ops
 
     @staticmethod
-    def _validate_problem_and_coeffs(problem: BaseProblem, mo_coeff: np.ndarray | None):
+    def _validate_problem_and_coeffs(problem: ElectronicStructureProblem, mo_coeff: np.ndarray | None):
         """Ensure the input problem can be translated to the MO basis."""
-        if not isinstance(problem, ElectronicStructureProblem):
-            raise TypeError(
-                "EntanglementForgingGroundStateSolver only supports ElectronicStructureProblem."
-            )
         if (problem.basis != ElectronicBasis.MO) and (mo_coeff is None):
             raise ValueError(
                 f"Cannot transform integrals to MO basis. The input problem is in the ({problem.basis}) basis, "
