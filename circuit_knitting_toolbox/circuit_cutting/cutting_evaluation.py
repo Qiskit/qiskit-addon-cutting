9fdf68739d6659667ed1b13db098c4f83a54a30f
Caleb Johnson <calebj1524@outlook.com>
2023-05-12 08:17:48 -0500

    Add separate_circuit, qpd_roundtrip test, and observable utils (#145)
    * Add a separate_circuit function + tests
    
    * Begin PR for separate_circuit
    
    * updates to init and test
    
    * observable_estimation module
    
    
    Co-authored-by: Jim Garrison <jim@garrison.cc>

---
+++ circuit_knitting_toolbox/circuit_cutting/cutting_evaluation.py
@@ -0,0 +1,86 @@
+# This code is a Qiskit project.
+
+# (C) Copyright IBM 2023.
+
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""Functions for evaluating circuit cutting experiments."""
+
+from __future__ import annotations
+
+from collections.abc import Sequence
+
+from qiskit.circuit import QuantumCircuit, ClassicalRegister
+from ..utils.observable_grouping import CommutingObservableGroup
+
+
+def append_measurement_circuit(
+    qc: QuantumCircuit,
+    cog: CommutingObservableGroup,
+    /,
+    *,
+    qubit_locations: Sequence[int] | None = None,
+    inplace: bool = False,
+) -> QuantumCircuit:
+    """Append a new classical register and measurement instructions for the given ``CommutingObservableGroup``.
+
+    The new register will be named ``"observable_measurements"`` and will be
+    the final register in the returned circuit, i.e. ``retval.cregs[-1]``.
+
+    Args:
+        - qc (QuantumCircuit): The quantum circuit.
+        - cog (CommutingObservableGroup): The commuting observable set for
+            which to construct measurements.
+        - qubit_locations (Sequence[int]): A ``Sequence`` whose length is the
+            number of qubits in the observables, where each element holds that
+            qubit's corresponding index in the circuit.  By default, the
+            circuit and observables are assumed to have the same number of
+            qubits, and the idenity map (i.e., ``range(qc.num_qubits)``) is
+            used.
+        - inplace (bool): Whether to operate on the circuit in place (default: False).
+
+    Returns:
+        - (QuantumCircuit): The new or modified circuit.
+
+    """
+    if qubit_locations is None:
+        # By default, the identity map.
+        if qc.num_qubits != cog.general_observable.num_qubits:
+            raise ValueError(
+                f"Quantum circuit qubit count ({qc.num_qubits}) does not match qubit "
+                f"count of observable(s) ({cog.general_observable.num_qubits}).  "
+                f"Try providing `qubit_locations` explicitly."
+            )
+        qubit_locations = range(cog.general_observable.num_qubits)
+    else:
+        if len(qubit_locations) != cog.general_observable.num_qubits:
+            raise ValueError(
+                f"qubit_locations has {len(qubit_locations)} element(s) but the "
+                f"observable(s) have {cog.general_observable.num_qubits} qubit(s)."
+            )
+    if not inplace:
+        qc = qc.copy()
+
+    # Append the appropriate measurements to qc
+    obs_creg = ClassicalRegister(len(cog.pauli_indices), name="observable_measurements")
+    qc.add_register(obs_creg)
+    # Implement the necessary basis rotations and measurements, as
+    # in BackendEstimator._measurement_circuit().
+    genobs_x = cog.general_observable.x
+    genobs_z = cog.general_observable.z
+    for clbit, subqubit in enumerate(cog.pauli_indices):
+        # subqubit is the index of the qubit in the subsystem.
+        # actual_qubit is its index in the system of interest (if different).
+        actual_qubit = qubit_locations[subqubit]
+        if genobs_x[subqubit]:
+            if genobs_z[subqubit]:
+                qc.sdg(actual_qubit)
+            qc.h(actual_qubit)
+        qc.measure(actual_qubit, obs_creg[clbit])
+
+    return qc
