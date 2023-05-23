a5763d3b09c7514550831df68dcbf7f737e50ea5
Jim Garrison <garrison@ibm.com>
2023-05-23 10:58:44 -0400

    Specify path of nitpick-exceptions file

--- docs/conf.py
+++ docs/conf.py
@@ -80,7 +80,7 @@ exclude_patterns = [
 # Enable nitpicky mode, and read from nitpick-exceptions file
 nitpicky = True
 nitpick_ignore = []
-with open("nitpick-exceptions") as f:
+with open(Path(__file__).parent / "nitpick-exceptions") as f:
     for line in f:
         line = line.strip()
         if line and not line.startswith("#"):
