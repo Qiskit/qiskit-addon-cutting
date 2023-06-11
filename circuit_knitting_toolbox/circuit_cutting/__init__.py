26f123b933e39ad20a332c4ae70471eeb6da0f02
Caleb Johnson <calebj1524@outlook.com>
2023-06-11 09:02:22 -0500

    Bring changes from remote

--- circuit_knitting_toolbox/circuit_cutting/__init__.py
+++ circuit_knitting_toolbox/circuit_cutting/__init__.py
@@ -14,7 +14,66 @@
 import sys
 from warnings import warn
 
-from circuit_knitting.cutting import (
+"""
+Circuit Cutting
+===============
+
+.. autosummary::
+    :toctree: ../stubs/
+    :nosignatures:
+
+    partition_circuit_qubits
+    partition_problem
+    cut_gates
+    execute_experiments
+    reconstruct_expectation_values
+
+.. autosummary::
+    :toctree: ../stubs/
+    :nosignatures:
+    :template: autosummary/class_no_inherited_members.rst
+
+    PartitionedCuttingProblem
+    CuttingExperimentResults
+
+Quasi-Probability Decomposition (QPD)
+=====================================
+
+.. autosummary::
+    :toctree: ../stubs/
+    :nosignatures:
+    :template: autosummary/class_no_inherited_members.rst
+
+    qpd.QPDBasis
+    qpd.BaseQPDGate
+    qpd.SingleQubitQPDGate
+    qpd.TwoQubitQPDGate
+    qpd.WeightType
+
+.. autosummary::
+    :toctree: ../stubs/
+    :nosignatures:
+
+    qpd.generate_qpd_samples
+    qpd.decompose_qpd_instructions
+
+CutQC
+=====
+
+.. autosummary::
+   :toctree: ../stubs/
+   :nosignatures:
+
+    cutqc.run_subcircuit_instances
+    cutqc.generate_summation_terms
+    cutqc.build
+    cutqc.verify
+    cutqc.cut_circuit_wires
+    cutqc.evaluate_subcircuits
+    cutqc.reconstruct_full_distribution
+"""
+
+from .cutting_decomposition import (
     partition_circuit_qubits,
     partition_problem,
     cut_gates,
