c10d960fa3744060b2c8f7163bfae76768e7e533
Caleb Johnson <calebj1524@outlook.com>
2023-06-30 17:01:25 -0500

    Update test/cutting/qpd/test_qpd.py
    black fix

--- test/cutting/qpd/test_qpd.py
+++ test/cutting/qpd/test_qpd.py
@@ -286,6 +286,19 @@ class TestQPDFunctions(unittest.TestCase):
     def test_supported_gates(self):
         gates = supported_gates()
         self.assertEqual(
-            {"rxx", "ryy", "rzz", "crx", "cry", "crz", "cx", "cy", "cz", "ch", "csx", "ecr"},
+            {   
+                "rxx",
+                "ryy",
+                "rzz",
+                "crx",
+                "cry",
+                "crz",
+                "cx",
+                "cy",
+                "cz",
+                "ch",
+                "csx",
+                "ecr",
+            },
             gates,
         )
