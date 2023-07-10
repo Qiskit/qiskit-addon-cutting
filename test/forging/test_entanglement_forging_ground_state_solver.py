ad0b172bd1fda933b58ceb2f84f3684c8e6a8f24
Caleb Johnson <calebj1524@outlook.com>
2023-07-10 16:37:56 -0500

    maxiter=0

--- test/forging/test_entanglement_forging_ground_state_solver.py
+++ test/forging/test_entanglement_forging_ground_state_solver.py
@@ -138,7 +138,7 @@ class TestEntanglementForgingGroundStateSolver(unittest.TestCase):
             bitstrings_u=bitstrings,
         )
 
-        optimizer = COBYLA(maxiter=1)
+        optimizer = COBYLA(maxiter=0)
         initial_point = [-0.83604922, -0.87326138, -0.93964018, 0.55224467]
         solver = EntanglementForgingGroundStateSolver(
             ansatz=ansatz,
