7d6f3c98b00cb69f3665dbc4e9ff665e04b81f81
Jim Garrison <jim@garrison.cc>
2023-09-10 21:36:45 -0400

    Add test

--- test/cutting/test_cutting_workflows.py
+++ test/cutting/test_cutting_workflows.py
@@ -11,16 +11,22 @@
 
 """Tests of various cutting workflows, particularly with regard to flexibility in transpilation."""
 
+import pytest
 from copy import deepcopy
 
 from qiskit.circuit.library import EfficientSU2, CXGate
 from qiskit.quantum_info import PauliList
 from qiskit.transpiler.preset_passmanagers import generate_preset_pass_manager
 from qiskit.providers.fake_provider import FakeLagosV2
+from qiskit_aer.primitives import Sampler
 
 from circuit_knitting.cutting.qpd.instructions import SingleQubitQPDGate
 from circuit_knitting.cutting.qpd import QPDBasis
-from circuit_knitting.cutting import partition_problem
+from circuit_knitting.cutting import (
+    partition_problem,
+    generate_cutting_experiments,
+    reconstruct_expectation_values,
+)
 
 
 def test_transpile_before_realizing_basis_id():
@@ -48,3 +54,48 @@ def test_transpile_before_realizing_basis_id():
         label: pass_manager.run(subcircuits["A"])
         for label, circuit in subcircuits.items()
     }
+
+
+@pytest.mark.parametrize(
+    "label1,label2",
+    [
+        ("foo", frozenset({1, 2, 4})),
+        (42, "bar"),
+    ],
+)
+def test_exotic_labels(label1, label2):
+    """Test workflow with labels of non-uniform type."""
+    circuit = EfficientSU2(4, entanglement="linear", reps=2).decompose()
+    circuit.assign_parameters([0.8] * len(circuit.parameters), inplace=True)
+    observables = PauliList(["ZZII", "IZZI", "IIZZ", "XIXI", "ZIZZ", "IXIX"])
+    subcircuits, bases, subobservables = partition_problem(
+        circuit=circuit,
+        partition_labels=[label1, label1, label2, label2],
+        observables=observables,
+    )
+    assert set(subcircuits.keys()) == {label1, label2}
+
+    subexperiments, coefficients = generate_cutting_experiments(
+        subcircuits, subobservables, num_samples=1500
+    )
+    assert subexperiments.keys() == subcircuits.keys()
+
+    samplers = {
+        label1: Sampler(run_options={"shots": 10}),
+        label2: Sampler(run_options={"shots": 10}),
+    }
+    results = {
+        label: sampler.run(subexperiments[label]).result()
+        for label, sampler in samplers.items()
+    }
+
+    for label in results:
+        for i, subexperiment in enumerate(subexperiments[label]):
+            results[label].metadata[i]["num_qpd_bits"] = len(subexperiment.cregs[0])
+
+    simulated_expvals = reconstruct_expectation_values(
+        results,
+        coefficients,
+        subobservables,
+    )
+    assert len(simulated_expvals) == len(observables)
