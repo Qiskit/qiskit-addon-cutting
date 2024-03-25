0cdf7dfae8b8d0e5b74874ce248a73c13ba7d121
Caleb Johnson <calebj1524@outlook.com>
2024-03-25 18:52:40 -0500

    Ignore CutBothWires for coverage

--- circuit_knitting/cutting/find_cuts.py
+++ circuit_knitting/cutting/find_cuts.py
@@ -118,6 +118,7 @@ def find_cuts(
             CircuitInstruction(CutWire(), [circuit.data[inst_id].qubits[qubit_id]], []),
         )
         counter += 1
+        # pragma: no cover
         if action.action.get_name() == "CutBothWires":
             # There should be two wires specified in the action in this case
             assert len(action.args) == 2
@@ -129,6 +130,7 @@ def find_cuts(
                 ),
             )
             counter += 1
+        # pragma: no cover end
 
     # Return metadata describing the cut scheme
     metadata: dict[str, Any] = {"cuts": []}
