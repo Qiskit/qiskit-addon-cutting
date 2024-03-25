74286c4ef2dac6944edc64d2561463df01a4a5ee
Jim Garrison <garrison@ibm.com>
2024-03-25 16:20:27 -0400

    Add support for `SamplerV2` (#507)
    * Add support for `SamplerV2`
    
    Fixes #488
    
    * Use qiskit branch with fixed `BackendSamplerV2`
    
    * Bump qiskit-aer version; restore qiskit version bounds
    
    * Update pyproject.toml
    
    * Update pyproject.toml
    
    * Update pyproject.toml
    
    * Bump nbmake minimum version
    
    nbmake < 1.4.3 depends on pydantic < 2.0 (see
    https://github.com/treebeardtech/nbmake/pull/109), but
    qiskit-ibm-runtime implicitly requires pydantic > 2.5
    (see https://github.com/Qiskit/qiskit-ibm-runtime/pull/1539).
    
    * Revert "Update pyproject.toml"
    
    This reverts commit b4de1c2a71d4142a8eb3c1fc17a505321c229e49.
    
    * Update README, pyproject.toml
    
    * Mark SamplerV2 branch as `pragma: no cover`
    
    This annotation can be removed once qiskit 1.1 is released
    
    * Updates
    
    * Tweak release note

--- circuit_knitting/cutting/cutting_reconstruction.py
+++ circuit_knitting/cutting/cutting_reconstruction.py
@@ -17,7 +17,10 @@ from collections.abc import Sequence, Hashable
 
 import numpy as np
 from qiskit.quantum_info import PauliList
-from qiskit.primitives import SamplerResult
+from qiskit.primitives import (
+    SamplerResult,  # for SamplerV1
+    PrimitiveResult,  # for SamplerV2
+)
 
 from ..utils.observable_grouping import CommutingObservableGroup, ObservableCollection
 from ..utils.bitwise import bit_count
@@ -27,7 +30,11 @@ from .qpd import WeightType
 
 
 def reconstruct_expectation_values(
-    results: SamplerResult | dict[Hashable, SamplerResult],
+    results: (
+        SamplerResult
+        | PrimitiveResult
+        | dict[Hashable, SamplerResult | PrimitiveResult]
+    ),
     coefficients: Sequence[tuple[float, WeightType]],
     observables: PauliList | dict[Hashable, PauliList],
 ) -> list[float]:
@@ -68,9 +75,11 @@ def reconstruct_expectation_values(
         ValueError: ``observables`` and ``results`` are of incompatible types.
         ValueError: An input observable has a phase not equal to 1.
     """
-    if isinstance(observables, PauliList) and not isinstance(results, SamplerResult):
+    if isinstance(observables, PauliList) and not isinstance(
+        results, (SamplerResult, PrimitiveResult)
+    ):
         raise ValueError(
-            "If observables is a PauliList, results must be a SamplerResult instance."
+            "If observables is a PauliList, results must be a SamplerResult or PrimitiveResult instance."
         )
     if isinstance(observables, dict) and not isinstance(results, dict):
         raise ValueError(
@@ -84,7 +93,7 @@ def reconstruct_expectation_values(
         subobservables_by_subsystem = decompose_observables(
             observables, "A" * len(observables[0])
         )
-        results_dict: dict[Hashable, SamplerResult] = {"A": results}
+        results_dict: dict[Hashable, SamplerResult | PrimitiveResult] = {"A": results}
         expvals = np.zeros(len(observables))
 
     else:
@@ -107,10 +116,28 @@ def reconstruct_expectation_values(
             subsystem_expvals = [
                 np.zeros(len(cog.commuting_observables)) for cog in so.groups
             ]
+            current_result = results_dict[label]
             for k, cog in enumerate(so.groups):
-                quasi_probs = results_dict[label].quasi_dists[i * len(so.groups) + k]
-                for outcome, quasi_prob in quasi_probs.items():
-                    subsystem_expvals[k] += quasi_prob * _process_outcome(cog, outcome)
+                idx = i * len(so.groups) + k
+                if isinstance(current_result, SamplerResult):
+                    # SamplerV1 provides a SamplerResult
+                    quasi_probs = current_result.quasi_dists[idx]
+                    for outcome, quasi_prob in quasi_probs.items():
+                        subsystem_expvals[k] += quasi_prob * _process_outcome(
+                            cog, outcome
+                        )
+                else:
+                    # SamplerV2 provides a PrimitiveResult
+                    data_pub = current_result[idx].data
+                    obs_array = data_pub.observable_measurements.array
+                    qpd_array = data_pub.qpd_measurements.array
+                    shots = qpd_array.shape[0]
+                    for j in range(shots):
+                        obs_outcomes = int.from_bytes(obs_array[j], "big")
+                        qpd_outcomes = int.from_bytes(qpd_array[j], "big")
+                        subsystem_expvals[k] += (1 / shots) * _process_outcome_v2(
+                            cog, obs_outcomes, qpd_outcomes
+                        )
 
             for k, subobservable in enumerate(subobservables_by_subsystem[label]):
                 current_expvals[k] *= np.mean(
@@ -140,19 +167,38 @@ def _process_outcome(
     num_meas_bits = len(_get_pauli_indices(cog))
 
     outcome = _outcome_to_int(outcome)
-    meas_outcomes = outcome & ((1 << num_meas_bits) - 1)
+    obs_outcomes = outcome & ((1 << num_meas_bits) - 1)
     qpd_outcomes = outcome >> num_meas_bits
 
+    return _process_outcome_v2(cog, obs_outcomes, qpd_outcomes)
+
+
+def _process_outcome_v2(
+    cog: CommutingObservableGroup, obs_outcomes: int, qpd_outcomes: int, /
+) -> np.typing.NDArray[np.float64]:
+    """
+    Process a single outcome of a QPD experiment with observables.
+
+    Args:
+        cog: The observable set being measured by the current experiment
+        obs_outcomes: An integer containing the outcome bits of the ``observable_measurements`` register
+        qpd_outcomes: An integer containing the outcome bits of the ``qpd_measurements`` register
+
+    Returns:
+        A 1D array of the observable measurements.  The elements of
+        this vector correspond to the elements of ``cog.commuting_observables``,
+        and each result will be either +1 or -1.
+    """
     # qpd_factor will be -1 or +1, depending on the overall parity of qpd
     # measurements.
     qpd_factor = 1 - 2 * (bit_count(qpd_outcomes) & 1)
 
     rv = np.zeros(len(cog.pauli_bitmasks))
     for i, mask in enumerate(cog.pauli_bitmasks):
-        # meas will be -1 or +1, depending on the measurement
+        # obs will be -1 or +1, depending on the measurement
         # of the current operator.
-        meas = 1 - 2 * (bit_count(meas_outcomes & mask) & 1)
-        rv[i] = qpd_factor * meas
+        obs = 1 - 2 * (bit_count(obs_outcomes & mask) & 1)
+        rv[i] = qpd_factor * obs
 
     return rv
 
--- test/cutting/test_cutting_reconstruction.py
+++ test/cutting/test_cutting_reconstruction.py
@@ -15,9 +15,14 @@ from ddt import ddt, data, unpack
 import pytest
 import numpy as np
 from qiskit.result import QuasiDistribution
-from qiskit.primitives import SamplerResult
+from qiskit.primitives import (
+    SamplerResult,
+    PrimitiveResult,
+    PubResult,
+    BitArray,
+)
+from qiskit.primitives.containers import make_data_bin
 from qiskit.quantum_info import Pauli, PauliList
-from qiskit.circuit import QuantumCircuit, ClassicalRegister
 
 from circuit_knitting.utils.observable_grouping import CommutingObservableGroup
 from circuit_knitting.cutting.qpd import WeightType
@@ -40,11 +45,6 @@ class TestCuttingReconstruction(unittest.TestCase):
                 quasi_dists=[QuasiDistribution({"0": 1.0})], metadata=[{}]
             )
             weights = [(1.0, WeightType.EXACT)]
-            subexperiments = [QuantumCircuit(2)]
-            creg1 = ClassicalRegister(2, name="observable_measurements")
-            creg2 = ClassicalRegister(1, name="qpd_measurements")
-            subexperiments[0].add_register(creg1)
-            subexperiments[0].add_register(creg2)
             observables = PauliList(["ZZ"])
             expvals = reconstruct_expectation_values(results, weights, observables)
             self.assertEqual([1.0], expvals)
@@ -53,7 +53,6 @@ class TestCuttingReconstruction(unittest.TestCase):
                 quasi_dists=[QuasiDistribution({"0": 1.0})], metadata=[{}]
             )
             weights = [(0.5, WeightType.EXACT), (0.5, WeightType.EXACT)]
-            subexperiments = {"A": QuantumCircuit(2)}
             observables = {"A": PauliList(["Z"]), "B": PauliList(["Z"])}
             with pytest.raises(ValueError) as e_info:
                 reconstruct_expectation_values(results, weights, observables)
@@ -67,18 +66,13 @@ class TestCuttingReconstruction(unittest.TestCase):
                 reconstruct_expectation_values(results2, weights, observables)
             assert (
                 e_info.value.args[0]
-                == "If observables is a PauliList, results must be a SamplerResult instance."
+                == "If observables is a PauliList, results must be a SamplerResult or PrimitiveResult instance."
             )
         with self.subTest("Test unsupported phase"):
             results = SamplerResult(
                 quasi_dists=[QuasiDistribution({"0": 1.0})], metadata=[{}]
             )
             weights = [(0.5, WeightType.EXACT)]
-            subexperiments = [QuantumCircuit(2)]
-            creg1 = ClassicalRegister(2, name="observable_measurements")
-            creg2 = ClassicalRegister(1, name="qpd_measurements")
-            subexperiments[0].add_register(creg1)
-            subexperiments[0].add_register(creg2)
             observables = PauliList(["iZZ"])
             with pytest.raises(ValueError) as e_info:
                 reconstruct_expectation_values(results, weights, observables)
@@ -87,7 +81,6 @@ class TestCuttingReconstruction(unittest.TestCase):
                 == "An input observable has a phase not equal to 1."
             )
             results = {"A": results}
-            subexperiments = {"A": subexperiments}
             observables = {"A": observables}
             with pytest.raises(ValueError) as e_info:
                 reconstruct_expectation_values(results, weights, observables)
@@ -95,6 +88,28 @@ class TestCuttingReconstruction(unittest.TestCase):
                 e_info.value.args[0]
                 == "An input observable has a phase not equal to 1."
             )
+        with self.subTest("Test SamplerV2 result"):
+            data_bin_cls = make_data_bin(
+                [("observable_measurements", BitArray), ("qpd_measurements", BitArray)],
+                shape=(),
+            )
+            obs_data = BitArray(
+                np.array([0, 0, 0, 1, 0, 0, 2, 3, 1, 0], dtype=np.uint8).reshape(-1, 1),
+                2,
+            )
+            qpd_data = BitArray(
+                np.array([0, 1, 1, 3, 2, 0, 1, 3, 0, 2], dtype=np.uint8).reshape(-1, 1),
+                2,
+            )
+            data_bin = data_bin_cls(
+                observable_measurements=obs_data, qpd_measurements=qpd_data
+            )
+            pub_result = PubResult(data_bin)
+            results = PrimitiveResult([pub_result])
+            weights = [(1.0, WeightType.EXACT)]
+            observables = PauliList(["II", "IZ", "ZI", "ZZ"])
+            expvals = reconstruct_expectation_values(results, weights, observables)
+            assert expvals == pytest.approx([0.0, -0.6, 0.0, -0.2])
 
     @data(
         ("000", [1, 1, 1]),
